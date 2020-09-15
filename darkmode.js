280813dcd0151bc489fa191a8877efe633480afb
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2020-09-15 22:07:58 +0900

    Theme

--- darkmode.js
+++ darkmode.js
@@ -4,25 +4,42 @@ const sun = document.querySelector("#sun-icon");
 const darktext = document.querySelector("#theme-button__btext");
 const whitetext = document.querySelector("#theme-button__wtext");
 
-let theme_mode = "light";
-
-// 컬러테마를 바꿔주는 함수
-function changeTheme() {
-  if (theme_mode === "light") {
+function firstLoad() {
+  if (localStorage.getItem("theme") === "dark") {
     moon.classList.add("hidden");
     sun.classList.remove("hidden");
     document.body.classList.add("darkmode");
     darktext.classList.add("hidden");
     whitetext.classList.remove("hidden");
-    theme_mode = "dark";
   } else {
     moon.classList.remove("hidden");
     sun.classList.add("hidden");
     darktext.classList.remove("hidden");
     whitetext.classList.add("hidden");
     document.body.classList.remove("darkmode");
-    theme_mode = "light";
   }
 }
 
+// 컬러테마를 바꿔주는 함수
+function changeTheme() {
+  if (localStorage.getItem("theme") === "light") {
+    moon.classList.add("hidden");
+    sun.classList.remove("hidden");
+    document.body.classList.add("darkmode");
+    darktext.classList.add("hidden");
+    whitetext.classList.remove("hidden");
+    localStorage.setItem("theme", "dark");
+  } else {
+    moon.classList.remove("hidden");
+    sun.classList.add("hidden");
+    darktext.classList.remove("hidden");
+    whitetext.classList.add("hidden");
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+window.addEventListener("DOMContentLoaded", firstLoad);
 theme_btn.addEventListener("click", changeTheme);
+
+// 얘들아 화이팅  😎화이팅!
